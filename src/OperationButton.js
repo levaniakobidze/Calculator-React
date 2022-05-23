ca41d853cc3ae6bb08b4edcabcee7248e4e689ce
levaniakobidze <levaniakobidze25@gmail.com>
2022-05-23 17:02:33 +0400

    asdasdas

---
+++ src/OperationButton.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { ACTIONS } from "./App";
+
+function OperationButton({ dispatch, operation }) {
+  return (
+    <button
+      className='operation-btn'
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }>
+      {operation}
+    </button>
+  );
+}
+
+export default OperationButton;
